ef23c9d28838f806ffaeb50b5857979f6e1c55d2
Gary <gary.coltrane1@marist.edu>
2017-08-13 13:45:12 -0400

    added some touches

--- public/script.js
+++ public/script.js
@@ -7,6 +7,25 @@ $(function(){
   });
 
 
-  
+
+
+$(".contact-form").submit(function(e){
+
+e.preventDefault();
+  var data = $(".contact-form").serialize();
+
+  $.ajax({
+  type: "POST",
+  url: "/sendEmail",
+  data: data,
+  success: function(data){
+    $(".contact-form").replaceWith("<h3> Thank you for posting an inquiry with KOP Communications. We promise to return your request as soon as possible. </h3>")
+  },
+  error: function(data){
+    $(".contact-form").replaceWith("<h3> There was an error processing your request, please refresh the page and try again. </h3>")
+  }
+});
+
+});
 
 });
--- routes/routes.js
+++ routes/routes.js
@@ -6,36 +6,44 @@ var nodemailer = require("nodemailer");
 module.exports = function(app) {
 
 
+
+
     app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname, '../views', 'index.html'));
     });
-    app.post('/sendEmail', function (req, res){
+    app.post('/sendEmail', function (req, res,err){
+      console.log(req.body)
 
-      var transporter = nodemailer.createTransport({
-        host:'smtp.gmail.com',
-        port:465,
-        secure: true,
-        service:'gmail',
-        auth:{
-          user: "",
-          pass:''
-        }
+      if(err){
+        res.send(err);
+      }
 
-      });
-      var mailOptions = {
-    from: '',
-    to: '',
-    subject: 'KOP Inquiry',
-    text: ''
-  };
-
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+  //     var transporter = nodemailer.createTransport({
+  //       host:'smtp.gmail.com',
+  //       port:465,
+  //       secure: true,
+  //       service:'gmail',
+  //       auth:{
+  //         user: kopEmail,
+  //         pass:pass
+  //       }
+  //
+  //     });
+  //     var mailOptions = {
+  //   from: '',
+  //   to: '',
+  //   subject: 'KOP Inquiry: ' + req.body.email + ": " + req.body.name,
+  //   text: req.body.message
+  // };
+  //
+  // transporter.sendMail(mailOptions, function(error, info){
+  //   if (error) {
+  //     console.log(error);
+  //   } else {
+  //     console.log('Email sent: ' + info.response);
+  //   }
+  // });
+  res.end();
 
     });
 
