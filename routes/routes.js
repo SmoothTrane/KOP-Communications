e96cb49f5565d88df6f4af29f3f503c0bd7e57a6
Gary <gary.coltrane1@marist.edu>
2017-08-14 21:05:28 -0400

    updated UI and email system

--- routes/routes.js
+++ routes/routes.js
@@ -1,48 +1,50 @@
 'use strict';
 var path = require('path');
 var nodemailer = require("nodemailer");
+var key = require("../key");
 
 
 module.exports = function(app) {
-
-
+var kopEmail = "contact.kopcommunications@gmail.com"
+var kopPass = key;
 
 
     app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname, '../views', 'index.html'));
     });
     app.post('/sendEmail', function (req, res,err){
-      console.log(req.body)
+      console.log(kopPass.pass);
+
 
       if(err){
         res.send(err);
       }
 
-  //     var transporter = nodemailer.createTransport({
-  //       host:'smtp.gmail.com',
-  //       port:465,
-  //       secure: true,
-  //       service:'gmail',
-  //       auth:{
-  //         user: kopEmail,
-  //         pass:pass
-  //       }
-  //
-  //     });
-  //     var mailOptions = {
-  //   from: '',
-  //   to: '',
-  //   subject: 'KOP Inquiry: ' + req.body.email + ": " + req.body.name,
-  //   text: req.body.message
-  // };
-  //
-  // transporter.sendMail(mailOptions, function(error, info){
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log('Email sent: ' + info.response);
-  //   }
-  // });
+      var transporter = nodemailer.createTransport({
+        host:'smtp.gmail.com',
+        port:465,
+        secure: true,
+        service:'gmail',
+        auth:{
+          user: kopEmail,
+          pass: kopPass.pass
+        }
+
+      });
+      var mailOptions = {
+    from: kopEmail,
+    to: 'kopcommunications@gmail.com ',
+    subject: 'KOP Inquiry: ' + req.body.email + ": " + req.body.name,
+    text: req.body.message
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   res.end();
 
     });
